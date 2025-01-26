1cdc3bc3c03fc2e5b2948394b05455257b96d121
chaitanya-kadali <charankadaliaa@gmail.com>
2025-01-26 20:23:51 +0530

    modified name

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import './styles/Header.css';
+import React from "react";
+import "./styles/Header.css";
 export default function Header() {
-  function gochatbot()
-  {
-    window.location.href='/chatbot';
+  function gochatbot() {
+    window.location.href = "/chatbot";
   }
-  function gohome()
-  {
-    window.location.href='/';
+  function gohome() {
+    window.location.href = "/";
   }
   return (
-  
     <div className="home-head">
-      <div className='home-first'>
-        <div className='outer-img'>
-      <div className='home-img'>
-        
+      <div className="home-first">
+        <div className="outer-img">
+          <div className="home-img"></div>
+        </div>
+
+        <p className="home-name" onClick={gohome}>
+          SafeCyber
+        </p>
       </div>
-      </div>
-      
-    <p className='home-name' onClick={gohome}>Ayush 2.0</p>
+      <button className="heads-button" onClick={gochatbot}>
+        Chat Bot
+      </button>
     </div>
-    <button className='heads-button' onClick={gochatbot}>Chat Bot</button>
-   </div>
-   
-  )
+  );
 }
