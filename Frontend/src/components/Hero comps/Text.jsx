db3018a9cd07fd18a4794bd327075735b2d3a367
devaganeshvatturi <devavatturi@gmail.com>
2025-01-29 22:07:43 +0530

    added contentsensor comps

---
+++ Frontend/src/components/Hero comps/Text.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import '../styles/Text.css'
+const Text =()=>{
+    const [tdata,setData]=useState("");
+    const [sdata,setSdata]=useState("data");
+    let flag=false;
+    const handleChange=(e)=>{
+      setData(e.target.value);
+    }
+    const handleSubmit =async()=>{
+        try{
+         let response=await axios.post('',tdata);
+         // if(response.data.success)
+         // {
+         //   setSdata(response.data.sdata);
+         // flag=true;
+         // }
+        }
+        catch(e)
+        {
+         console.log(e);
+        }
+    }
+   return(
+    <div>
+     <input className='text-inp' onChange={handleChange} />
+     <p onClick={handleSubmit}>Submit</p>
+     { flag &&
+       <div>
+         <p>{sdata}</p>
+       </div>
+     }
+    </div>
+   );
+ }
+ export default Text;
