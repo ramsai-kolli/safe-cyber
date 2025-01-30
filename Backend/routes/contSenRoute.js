f9c23811ccf8b3d550bf7dfc73a9a3883d27dc03
chaitanya-kadali <charankadaliaa@gmail.com>
2025-01-31 01:47:38 +0530

    solved bug 1 : back cont sen

--- Backend/routes/contSenRoute.js
+++ Backend/routes/contSenRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { chatControl } = require("../controllers/contSenController");
+const { contControl} = require("../controllers/contSenController");
 
 const router = express.Router();
 
 // chat route
-router.route("/contsensor-text").post(chatControl);
+router.route("/contsensor-text").post(contControl);
 
 module.exports = router;
