1509cc7141b806c0b9230e7aeaa493c8f317e167
chaitanya-kadali <charankadaliaa@gmail.com>
2025-01-30 23:21:40 +0530

    add ntg

--- Backend/routes/chatRoute.js
+++ Backend/routes/chatRoute.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const {
-chatControl,
-} = require("../controllers/chatController");
+const { chatControl } = require("../controllers/chatController");
 
 const router = express.Router();
 
 // chat route
-router.route("/chat").post(chatControl); 
+router.route("/chat").post(chatControl);
 
 module.exports = router;
