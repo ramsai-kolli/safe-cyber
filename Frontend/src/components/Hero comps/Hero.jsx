2c71ab86a48f861d44d3a9fe89d916b2c6309a86
devaganeshvatturi <devavatturi@gmail.com>
2025-01-31 12:04:09 +0530

    added logout

--- Frontend/src/components/Hero comps/Hero.jsx
+++ Frontend/src/components/Hero comps/Hero.jsx
@@ -7,11 +7,15 @@ import ComplaintPortal from "./ComplaintPortal";
 import { useState } from "react";
 import '../styles/Hero.css'
 import Trending from "./Trending";
+import Logout from "../Separate Comps/Logout";
 function Hero({email}) {
+  let flag=true;
   if (email) {
     console.log("mail is",email);
+    flag=true;
   } else {
     console.log("Nothing received");
+    flag=false;
   }
   const [comp, setComp] = useState(1);
   let componentToDisplay;
@@ -35,28 +39,32 @@ function Hero({email}) {
         componentToDisplay = <AiChatBot />;  
       break;
     case 7:
-        componentToDisplay = <ComplaintPortal />; 
+        componentToDisplay = <ComplaintPortal email={email}/>; 
         break; 
     default:
       componentToDisplay = null;
   }
   return (
-    <div>
-      
+    <div className="home-main">
+      <div className="home-comp-bar">
   <div className="home-nav">
         <p onClick={()=>{setComp(1)}} className={comp === 1 ? "active-tab" : ""}>Home</p>
-        <p onClick={()=>{setComp(2)}} className={comp === 2 ? "active-tab" : ""}>AbuseSensor</p>
-        <p onClick={()=>{setComp(3)}} className={comp === 3 ? "active-tab" : ""}>Fact Chack</p>
+        <p onClick={()=>{setComp(2)}} className={comp === 2 ? "active-tab" : ""}>Abuse Censor</p>
+        <p onClick={()=>{setComp(3)}} className={comp === 3 ? "active-tab" : ""}>Fact Check</p>
         <p onClick={()=>{setComp(4)}} className={comp === 4 ? "active-tab" : ""}>Trending</p>
         <p onClick={()=>{setComp(5)}} className={comp === 5 ? "active-tab" : ""}>Group Chat</p>
         <p onClick={()=>{setComp(6)}} className={comp === 6 ? "active-tab" : ""}>Gemini Chatbot</p>
         <p onClick={()=>{setComp(7)}} className={comp === 7 ? "active-tab" : ""}>Complaint Portal</p>
         
       </div>
+      <div className="hero-logout">
+        { flag &&
+          <Logout/>
+        }
+      </div>
+      </div>
       <div>
-        {/* {
-      email!=null && <p>{email}</p> 
-        } */}
+        
      {componentToDisplay}
       </div>   
     </div>
